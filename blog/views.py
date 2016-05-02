82f8a8a573a704e27765093fc51c95f7f5cc46cc
AlexanderMukas <alexandermukas@yahoo.com>
2016-05-02 19:30:26 +0300

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 
-# Create your views here.
+# Добавляем модуль с текущим временем по час.поясу
+from django.utils import timezone
+# 02.05.2016 - Импортируем модель Post
+from .models import Post
+#
+
 # 02.05.2016 - Добавляем логику приложения. Вьюшки берут инфо
 # с модели, манипулируют, и передают шаблону результат
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	# Дописали posts. Выборка всех постов и сортировка по дате публикации
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	
+	return render(request, 'blog/post_list.html', {'posts': posts})
