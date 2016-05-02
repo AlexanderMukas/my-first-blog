1e799d10bddefd416901118eae5d6f5b205bc2aa
AlexanderMukas <alexandermukas@yahoo.com>
2016-05-02 15:54:27 +0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 # Create your views here.
+# 02.05.2016 - Добавляем логику приложения. Вьюшки берут инфо
+# с модели, манипулируют, и передают шаблону результат
+
+def post_list(request):
+	return render(request, 'blog/post_list.html', {})
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+# 02.05.2016 - Конфигурация URL адресов.
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -7,4 +9,8 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    
+    # 02.05.16 Домашняя страничка
+    url(r'', include('blog.urls')),
+    #
 ]
